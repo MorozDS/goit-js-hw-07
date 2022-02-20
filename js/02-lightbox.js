f2bfe632bf01de8980d8994bf4300f3b7e744ff6
MorozDS <morozddima@gmail.com>
2022-02-20 22:06:13 +0200

    Part  2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const listRef = document.querySelector(".gallery");
+
+const galleryList = galleryItems.map(item => `
+<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>
+
+`).join('');
+
+
+
+listRef.insertAdjacentHTML('beforeend', galleryList);
+listRef.addEventListener('click', galleryClick);
+
+function galleryClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return
+  };
+    new SimpleLightbox('.gallery a',
+    {
+    captionsData: 'alt',
+    captionDelay: 250,
+        }
+    );
+};
